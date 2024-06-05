1026e7ca7d5633b486b33272efa76f585668e318
Luis Perez <lperezdev843@gmail.com>
2024-06-05 02:44:28 -0400

    installed react, react router and created subroutes for main pages

---
+++ P1/Frontend/entertainment-app/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Movies from "./pages/Movies";
+import TvSeries from "./pages/TvSeries";
+import Bookmarked from "./pages/Bookmarked";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/tv-shows" element={<TvSeries />} />
+        <Route path="/bookmarked" element={<Bookmarked />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
