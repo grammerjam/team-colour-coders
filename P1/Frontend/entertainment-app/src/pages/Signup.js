7cac10e534545eadf33744ef8c088194f2348b19
Carmen Peterson <47091936+varsvisualizes@users.noreply.github.com>
2024-06-19 12:53:05 -0400

    Filled out Signup JS and CSS pages

--- P1/Frontend/entertainment-app/src/pages/Signup.js
+++ P1/Frontend/entertainment-app/src/pages/Signup.js
@@ -1,8 +1,33 @@
+import React from "react";
+import "./Signup.css";
+import {ReactComponent as Logo} from './logo.svg';
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div class="center-box">
+      <Logo />
+      <div className="container">
+        <form action="">
+          <h1>Sign Up</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Email address" required />
+          </div>
+          <div className="input-box">
+            <input type="text" placeholder="Password" required />
+          </div>
+          <div className="input-box">
+            <input type="text" placeholder="Repeat password" required />
+          </div>
+        </form>
+        <button type="submit">Create an account</button>
+        <div>
+          <p>
+            Already have an account? <a href="./login">Login</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
