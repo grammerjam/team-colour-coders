7004e878c2c747437dce25329e1a417cef3e6faf
poverty149 <parvathyuk1@gmail.com>
2024-07-02 18:57:40 -0400

    installed knex

--- P1/Backend/index.js
+++ P1/Backend/index.js
@@ -2,5 +2,7 @@ const mysql = require("mysql");
 const express = require("express");
 
 const app = express();
-
+app.get('/',(err,res)=>{
+    res.send("Hello");
+});
 app.listen(3000, () => console.log("Server is listening on port 3000...") );
