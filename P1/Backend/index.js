93d14a31bb6df06d83c739e1f85f168cb439b627
Ato Arkhurst <atoparkhurst@gmail.com>
2024-07-03 14:46:39 -0400

    Update index.js to connect to database

--- P1/Backend/index.js
+++ P1/Backend/index.js
@@ -1,6 +1,38 @@
-const mysql = require("mysql");
-const express = require("express");
+const mysql = require('mysql');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log("Server is listening on port 3000...") );
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, 'certificates', 'ca-certificate.pem'))
+    }
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+app.get('/test-db', (req, res) => {
+    connection.query('SELECT 1 + 1 AS solution', (error, results) => {
+        if (error) throw error;
+        res.send(`The solution is: ${results[0].solution}`);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+});
