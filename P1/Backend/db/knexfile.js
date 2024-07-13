14b55357e4ef48a7b8572e8b7ae932ad37c46f53
Carmen Vargas <47091936+varsvisualizes@users.noreply.github.com>
2024-07-12 21:49:31 -0400

    Sets a path for the migrations files to be stored

--- P1/Backend/db/knexfile.js
+++ P1/Backend/db/knexfile.js
@@ -3,19 +3,19 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
 module.exports = {
-  
   development: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
       host: '127.0.0.1',
       port: 3306,
       user: 'your_database_user',
       password: 'your_database_password',
       database: 'myapp_test',
+    },
+    migrations: {
+      directory: "./db/migrations",
     }
-  },
-
-  
-  
+  }
 };
